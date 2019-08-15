36b552a57c2fd7376475c6d3db048ff16f96ad56
Neil Ryan <nr110718@bloomenergy.com>
2019-08-15 16:59:36 -0700

    Hangman game flow under construction

--- action.js
+++ action.js
@@ -1,1 +1,41 @@
-alert("JS IS WORKING!");
+let display = document.getElementById('board');
+let data_input = document.getElementById('data_input');
+let submit_button = document.getElementById('submit');
+
+let board = [];
+
+submit_button.addEventListener('click', function() {
+    word = data_input.value;
+    convertToArray(word)
+})
+
+function convertToArray(word) {
+    word = word.split('');
+    makeBoardArray(word);
+}
+
+function makeBoardArray(array) {
+    board = new Array(array.length);
+    board.fill("_");
+    console.log(board);
+    displayBoard(board);
+};
+
+function displayBoard(array) {
+    display.innerHTML = "" + array.join(" ") + "";
+    data_input.value = "";
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
